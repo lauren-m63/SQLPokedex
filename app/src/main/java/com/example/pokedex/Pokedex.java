d81d2938137e6979ad53c185bc8ad369bd6905f0
lauren-m63 <lauren.m63@yahoo.com>
2025-10-17 20:50:37 -0500

    layout and some pokedex class changes

--- app/src/main/java/com/example/pokedex/Pokedex.java
+++ app/src/main/java/com/example/pokedex/Pokedex.java
@@ -1,5 +1,7 @@
 package com.example.pokedex;
 
+import java.util.LinkedList;
+
 public class Pokedex {
 
          int number;
@@ -8,7 +10,7 @@ public class Pokedex {
          String gender;
          double height;
          double weight;
-         int level;
+         LinkedList<Integer> level;
          int hp;
          int attack;
          int defense;
@@ -16,7 +18,7 @@ public class Pokedex {
 
     public Pokedex(int number, String name, String species,
                    String gender, double height, double weight,
-                   int level, int hp, int attack, int defense) {
+                   LinkedList<Integer> level, int hp, int attack, int defense) {
         this.number = number;
         this.name = name;
         this.species = species;
@@ -29,5 +31,118 @@ public class Pokedex {
         this.defense = defense;
 
 
-}
+} // end constructor
+
+    public Pokedex (){
+         number = 896;
+         name = "Glastrier";
+         species = "Wild Horse Pokemon";
+         gender = "Other";
+         height = 2.2;
+         weight = 800.0;
+         level = null;
+         hp = 0;
+         attack= 0;
+         defense= 0;
+
+    } // end default constructor
+
+
+    public int getNumber() {
+        return number;
     }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+
+    public String getSpecies() {
+        return species;
+    }
+
+    public void setSpecies(String species) {
+        this.species = species;
+    }
+
+
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+
+    public LinkedList<Integer> getLevel() {
+        return level;
+    }
+
+    public void setLevel(LinkedList<Integer> level) {
+        this.level = level;
+    }
+
+    public int getHp() {
+        return hp;
+    }
+
+    public void setHp(int hp) {
+        this.hp = hp;
+    }
+
+
+
+    public int getAttack() {
+        return attack;
+    }
+
+    public void setAttack(int attack) {
+        this.attack = attack;
+    }
+
+
+
+    public int getDefense() {
+        return defense;
+    }
+
+    public void setDefense(int defense) {
+        this.defense = defense;
+    }
+
+
+
+} // final bracket
