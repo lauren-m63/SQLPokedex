2b0c65cee864b5371e08610c7ecbe790633dc3ee
lauren-m63 <lauren.m63@yahoo.com>
2025-10-17 19:39:55 -0500

    changing the layout and trying to use table

---
+++ app/src/main/java/com/example/pokedex/Pokedex.java
@@ -0,0 +1,33 @@
+package com.example.pokedex;
+
+public class Pokedex {
+
+         int number;
+         String name;
+         String species;
+         String gender;
+         double height;
+         double weight;
+         int level;
+         int hp;
+         int attack;
+         int defense;
+
+
+    public Pokedex(int number, String name, String species,
+                   String gender, double height, double weight,
+                   int level, int hp, int attack, int defense) {
+        this.number = number;
+        this.name = name;
+        this.species = species;
+        this.gender = gender;
+        this.height = height;
+        this.weight = weight;
+        this.level = level;
+        this.hp = hp;
+        this.attack = attack;
+        this.defense = defense;
+
+
+}
+    }
---
+++ app/src/main/java/com/example/pokedex/PokedexMain.java
@@ -0,0 +1,73 @@
+package com.example.pokedex;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class PokedexMain extends AppCompatActivity {
+
+    EditText numberInput;
+    EditText nameInput;
+    EditText speciesInput;
+   int genderInput;
+    EditText heightInput;
+    EditText weightInput;
+    EditText HPInput;
+    EditText attackInput;
+    EditText defenseInput;
+    Button submitButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.layout); // Make sure res/layout/layout.xml exists
+
+        numberInput = findViewById(R.id.numberInput);
+        nameInput = findViewById(R.id.nameInput);
+        speciesInput = findViewById(R.id.speciesInput);
+        heightInput = findViewById(R.id.heightInput);
+        weightInput = findViewById(R.id.weightInput);
+        HPInput = findViewById(R.id.HPInput);
+        attackInput = findViewById(R.id.attackInput);
+        defenseInput = findViewById(R.id.defenseInput);
+       //genderInput = findViewById(genderInput);
+
+
+        submitButton  = findViewById(R.id.submitButton);
+
+
+
+      submitButton.setOnClickListener(v -> {
+          int number = Integer.parseInt(numberInput.getText().toString());
+          String name = nameInput.getText().toString();
+          String species = speciesInput.getText().toString();
+          double height = Double.parseDouble(heightInput.getText().toString());
+          double weight = Double.parseDouble(weightInput.getText().toString());
+          //int level = Integer.parseInt(levelSpinner.getSelectedItem().toString());
+          int hp = Integer.parseInt(HPInput.getText().toString());
+          int attack = Integer.parseInt(attackInput.getText().toString());
+          int defense = Integer.parseInt(defenseInput.getText().toString());
+
+
+
+          // Find that radio button
+
+          // Retrieve its text (e.g., "Male", "Female", "Unknown")
+          //String gender = selectedRadio.getText().toString();
+
+         // Pokedex entry = new Pokedex(
+                  //number, name, species, gender,
+                 // height, weight, 1, hp, attack, defense
+        //  );
+      });
+
+    }
+
+
+
+}
