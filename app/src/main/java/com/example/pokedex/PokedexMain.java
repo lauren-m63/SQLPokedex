d706c2830f771c3f2ffb875b26da9ea9bfc8b994
lauren-m63 <lauren.m63@yahoo.com>
2025-10-18 16:45:05 -0500

    starting the reset buttons and stuff

--- app/src/main/java/com/example/pokedex/PokedexMain.java
+++ app/src/main/java/com/example/pokedex/PokedexMain.java
@@ -1,6 +1,7 @@
 package com.example.pokedex;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioButton;
@@ -10,7 +11,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class PokedexMain extends AppCompatActivity {
 
-    EditText numberInput;
+    EditText nationalNumberInput;
     EditText nameInput;
     EditText speciesInput;
    int genderInput;
@@ -20,14 +21,15 @@ public class PokedexMain extends AppCompatActivity {
     EditText attackInput;
     EditText defenseInput;
     Button submitButton;
+    Button resetButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.linear_layout); // Make sure res/layout/layout.xml exists
+        setContentView(R.layout.table); // Make sure res/layout/layout.xml exists
 
-        numberInput = findViewById(R.id.nationalNumberInput);
+        nationalNumberInput = findViewById(R.id.nationalNumberInput);
         nameInput = findViewById(R.id.nameInput);
         speciesInput = findViewById(R.id.speciesInput);
         heightInput = findViewById(R.id.heightInput);
@@ -35,27 +37,39 @@ public class PokedexMain extends AppCompatActivity {
         HPInput = findViewById(R.id.HPInput);
         attackInput = findViewById(R.id.attackInput);
         defenseInput = findViewById(R.id.defenseInput);
+        //levelInput hi
        //genderInput = findViewById(genderInput);
-
-
         submitButton  = findViewById(R.id.submitButton);
+        resetButton = findViewById(R.id.resetButton);
+
+        String nationalNumberInputString = nationalNumberInput.getText().toString();
+        String nameInputString = nameInput.getText().toString();
+        String speciesInputString = speciesInput.getText().toString();
+        String heightInputString = heightInput.getText().toString();
+        String weightInputString = weightInput.getText().toString();
+        String hpInputString = HPInput.getText().toString();
+        String attackInputString = attackInput.getText().toString();
+        String defenseInputString = defenseInput.getText().toString();
+
+
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int number = Integer.parseInt(nationalNumberInput.getText().toString());
+                String name = nameInput.getText().toString();
+                String species = speciesInput.getText().toString();
+                double height = Double.parseDouble(heightInput.getText().toString());
+                double weight = Double.parseDouble(weightInput.getText().toString());
+                //int level = Integer.parseInt(levelSpinner.getSelectedItem().toString());
+                int hp = Integer.parseInt(HPInput.getText().toString());
+                int attack = Integer.parseInt(attackInput.getText().toString());
+                int defense = Integer.parseInt(defenseInput.getText().toString());
+
+            }
+        }); // end submit on Click listener
 
 
 
-      submitButton.setOnClickListener(v -> {
-          int number = Integer.parseInt(numberInput.getText().toString());
-          String name = nameInput.getText().toString();
-          String species = speciesInput.getText().toString();
-          double height = Double.parseDouble(heightInput.getText().toString());
-          double weight = Double.parseDouble(weightInput.getText().toString());
-          //int level = Integer.parseInt(levelSpinner.getSelectedItem().toString());
-          int hp = Integer.parseInt(HPInput.getText().toString());
-          int attack = Integer.parseInt(attackInput.getText().toString());
-          int defense = Integer.parseInt(defenseInput.getText().toString());
-
-
-
-          // Find that radio button
 
           // Retrieve its text (e.g., "Male", "Female", "Unknown")
           //String gender = selectedRadio.getText().toString();
@@ -64,10 +78,21 @@ public class PokedexMain extends AppCompatActivity {
                   //number, name, species, gender,
                  // height, weight, 1, hp, attack, defense
         //  );
-      });
-
-    }
 
 
+        resetButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                nationalNumberInput.setText("");
+            }
+        });
 
-}
+
+
+
+
+    } // end on create
+
+
+
+} // end class
